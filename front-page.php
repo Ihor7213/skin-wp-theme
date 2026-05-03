54d26feab88db44e6e05bcd4de6e909fbc06eb15
Ihor Berchuk <berchuk10@gmail.com>
2026-05-03 14:30:05 +0400

    feat: add front-page.php and assemble all sections

---
+++ front-page.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<main>
+  <?php get_template_part( 'template-parts/sections/hero' ); ?>
+  <?php get_template_part( 'template-parts/sections/features' ); ?>
+  <?php get_template_part( 'template-parts/sections/gallery' ); ?>
+  <?php get_template_part( 'template-parts/sections/about' ); ?>
+  <?php get_template_part( 'template-parts/sections/process' ); ?>
+  <?php get_template_part( 'template-parts/sections/artists' ); ?>
+  <?php get_template_part( 'template-parts/sections/reviews' ); ?>
+  <?php get_template_part( 'template-parts/sections/faq' ); ?>
+  <?php get_template_part( 'template-parts/sections/contact' ); ?>
+</main>
+
+<?php get_footer(); ?>
